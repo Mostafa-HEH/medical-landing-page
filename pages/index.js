6cde93e48a4c4695eaa45db7b28dd3d7be682b1a
Mostafa Hassan <mostafa813765@gmial.com>
2022-05-16 18:30:19 +0200

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,7 @@
 import Link from "next/link";
 
 const HomePage = () => {
-  return (
-    <div>
-      <nav>
-        <Link href="/" className="logo">
-          logo
-        </Link>
-        <ul className="nav-links">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/features">Features</Link>
-          </li>
-          <li>
-            <Link href="/services">Services</Link>
-          </li>
-          <li>
-            <Link href="/reviews/2022/1">Reviews</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link href="tel:1800789123">
-              <span>1800 789 123</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+  return <div>Home page</div>;
 };
 
 export default HomePage;
